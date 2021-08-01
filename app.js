49177305b4c7925dacd07558af185542e453a338
Olivia Lewis <ollybaby12@gmail.com>
2021-07-31 22:34:02 -0400

    strings

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ console.log(myName)
 myName = "Sandra Dee"
 console.log(address, zip, state)
 console.log(myName)
+
+const sent = 'Don\'t ignore the rules!'
+
+console.log(sent);
+
+const name = 'Olivia';
+const lastName = 'Lewis';
+let fullName = name + ' ' + lastName;
+
+console.log('Hi there! Your full name is : ' + fullName);
